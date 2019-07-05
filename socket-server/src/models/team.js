f292b21a7f5ffe918f3348510221891cbb1c823a
rbt <rbt@trifork.com>
2019-07-05 11:43:57 +0200

    implemented sidenav

--- socket-server/src/models/team.js
+++ socket-server/src/models/team.js
@@ -1,13 +1,9 @@
 const mongoose = require('mongoose');
 
 const teamSchema = new mongoose.Schema({
-  identifier : { type: String, unique: true, required: true},
-  inviteCode: { type: String, unique: true},
+  identifier : { type: Number, unique: true, required: true},
   longName: { type: String},
   users: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-  goalForValues: [{ type: mongoose.Schema.Types.ObjectId, ref: 'EventType' }],
-  goalAgainstValues: [{ type: mongoose.Schema.Types.ObjectId, ref: 'EventType' }],
-  assistForValues: [{ type: mongoose.Schema.Types.ObjectId, ref: 'EventType' }]
 });
 
 const Team = mongoose.model('Team', teamSchema);
